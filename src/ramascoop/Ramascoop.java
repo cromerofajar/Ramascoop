19f108930e5eb8a9b07eefa7a7523836844037db
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-13 10:06:48 +0100

    Comienzo ramas

---
+++ src/ramascoop/Ramascoop.java
@@ -0,0 +1,16 @@
+package ramascoop;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class Ramascoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este es el primer commit de la rama master.\n Aqui empiezan las ramas");
+    }
+    
+}
